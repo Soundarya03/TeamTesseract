1fc81002bf1ac1b183ffc2582e9fd9aec5f1a679
Soundarya03 <soundarya1103@gmail.com>
2020-09-19 16:25:13 +0530

    provisions for input

--- lib/backend/app.py
+++ lib/backend/app.py
@@ -1,15 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+import json
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+response = ''
 
+
+# PART-1 : Getting the user's input.
+@cross_origin()
+@app.route('/input', methods=['POST'])
+def seedRoute():
+    global response
+    request_data = request.data
+    request_data = json.loads(request_data.decode('utf-8'))
+    seed = request_data['seed']
+    response = seed
+    return " "
+
+
+# PART-2 : The variable 'response' can be used to...
+
+
+# PART-3 : Sending the ML model's output to the webpage.
 @cross_origin()
 @app.route('/output', methods=['GET'])
 def index():
-    return jsonify({'poem': 'Hi! Hope this worked.'})
+    return jsonify({'poem': response})
 
 
 if __name__ == "__main__":
